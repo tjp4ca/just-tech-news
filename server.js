e0b0c105671437ff84da25d5a1627d10c9a47be7
Michael Park <tjp4ca@gmail.com>
2022-08-12 23:21:00 -0400

    password hash

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-// const routes = require('./routes');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // // turn on routes
-// app.use(routes);
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
